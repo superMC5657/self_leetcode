a000a5c6f6bc8c4c88d15c8afff883b305a8c41c
zhangyan <18758266469@163.com>
2019-12-25 09:14:27 +0800

    add 98th code in tree dir

--- scripts/tree/0_main.cpp
+++ scripts/tree/0_main.cpp
@@ -3,7 +3,7 @@
   !@author: superMC @email: 18758266469@163.com
   !@fileName: 0_main.cpp
  */
-#include "94_binary-tree-inorder-traversal.cpp"
+#include "98_validate-binary-search-tree.cpp"
 
 int main() {
     fun();
--- scripts/tree/98_validate-binary-search-tree.cpp
+++ scripts/tree/98_validate-binary-search-tree.cpp
@@ -3,3 +3,39 @@
   !@author: superMC @email: 18758266469@163.com
   !@fileName: 98_validate.cpp
  */
+#include <environment.h>
+#include <function.hpp>
+
+class Solution {
+private:
+    void inOrder(struct TreeNode *root, int arr[], int *len) {
+        if (root != NULL) {
+            inOrder(root->left, arr, len);
+            arr[(*len)++] = root->val;
+
+            inOrder(root->right, arr, len);
+        }
+    }
+
+public:
+    bool isValidBST(struct TreeNode *root) {
+        int arr[10000];
+        int len = 0;
+        inOrder(root, arr, &len);
+        for (int i = 0; i < len - 1; i++) {
+            if (arr[i] >= arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int fun() {
+    Solution solution;
+    TreeNode *treeNode = stringToTreeNode("[0,null,-1]");
+    bool ret = solution.isValidBST(treeNode);
+    printf("%s", ret ? "true" : "false");
+    return 0;
+}
+
